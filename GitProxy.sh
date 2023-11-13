ee387c1d937900f76eb683cb50e72ccc5d0a5d88
yw <ywsh9527@163.com>
2023-11-13 10:06:51 +0800

    feat: 颜色

--- GitProxy.sh
+++ GitProxy.sh
@@ -2,6 +2,7 @@
 
 # 操作类型
 operations=("设置代理" "取消代理")
+default_proxy="http://127.0.0.1:7890"
 
 # 快速修改git代理
 changeGitGlobalProxy() {
@@ -24,7 +25,10 @@ changeGitGlobalProxy() {
 # 设置代理
 setProxy() {
   read -p "请输入您的Git代理地址: " proxy_url
-  git config --global http.proxy $proxy_url 
+  if  [ ! -n "$proxy_url" ];then
+    proxy_url=$default_proxy
+  fi
+  git config --global http.proxy $proxy_url
   git config --global https.proxy $proxy_url
   checkProxy
 }
@@ -33,14 +37,14 @@ setProxy() {
 unsetProxy() {
   git config --global --unset http.proxy
   git config --global --unset https.proxy
-  echo "Git代理已取消."
+  echo -e "\e[93mGit代理已取消\e[0m"
 }
 
 # 查看代理
 checkProxy() {
   curProxyUrl=`git config --global --get http.proxy`
   # git config --global --get https.proxy
-  echo "Git当前代理为: $curProxyUrl"
+  echo -e "\e[93mGit当前代理为: $curProxyUrl\e[0m"
 }
 
 
